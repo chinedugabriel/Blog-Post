7893838968e43cf526ae34950c0cade22c8cc28d
Chinedu Gabriel <chinedugabriel29@gmail.com>
2024-05-24 11:32:41 +0100

    Style: change the view for nav and added logos and favicon needed

--- index.php
+++ index.php
@@ -7,27 +7,28 @@
     <!-- bootstrap link start here -->
     <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
     <!-- bootstrap link ends here -->
+    <link rel="shortcut icon" href="./src/img/favicon.ico" type="image/x-icon">
 </head>
 <body class="bg-primary">
     <section class="vh-100 d-flex justify-content-center align-items-center">
-        <div class="container-fluid p-3 border h-75 ">
+        <div class="container-fluid p-3 border h-md-75 ">
             <div class="row h-100">
-                <div class="col-md-2 h-100 border d-flex flex-column justify-content-around align-items-center">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <img src="" alt="logo" class="logo">
+                <div class="col-md-2 h-md-100 border d-flex flex-column justify-content-around align-items-center p-0 px-1">
+                    <div class="row w-75">
+                        <div class="col-md-12 text-center">
+                            <img src="./src/img/logo.png" alt="logo" class="img-fluid">
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-md-12">
-                            <ul class="list-unstyled">
-                                <li class="">
-                                    <a href="#" class="text-white">
+                    <div class="row w-100 p-0 h-25">
+                        <div class="col-md-12 p-0 ">
+                            <ul class="list-unstyled h-100 py-3 d-flex flex-column justify-content-around ">
+                                <li class="text-center border ">
+                                    <a href="#" class="text-white text-decoration-none">
                                         All Post
                                     </a>
                                 </li>
-                                <li class="">
-                                    <a href="#" class="text-white">
+                                <li class=" text-center border border-danger">
+                                    <a href="#" class="text-white text-decoration-none">
                                         Add Post
                                     </a>
                                 </li>
@@ -48,10 +49,10 @@
                     </div>
                 </div>
                 <div class="col-md-9 offset-md-1">
-                    <form action="" method="" class="d-grid gap-1 p-3 h-100">
+                    <form action="" method="" class="d-grid gap-1 p-3 pt-0 h-100">
                         <div class="row">
                             <div class="col-md-12 text-center">
-                                <p class="display-3 text-white">Post Content</p>
+                                <p class="display-5 text-white">Post Content</p>
                             </div>
                         </div>
                         <div class="row border">
@@ -60,10 +61,10 @@
                                     <p class="display-5 text-white m-0">
                                         Title
                                     </p>
-                                    <input type="text" class="form-control" name="" id="" placeholder="Title">
+                                    <input type="text" class="form-control rounded rounded-0" name="" id="" placeholder="Title">
                                 </div>
                                 <div class="row">
-                                    <textarea class="form-control" name="" id="" rows="10" cols="" placeholder="Description"></textarea>
+                                    <textarea class="form-control rounded rounded-0" name="" id="" rows="10" cols="" placeholder="Description"></textarea>
                                 </div>
                             </div>
                             <div class="col-md-4 d-flex flex-column gap-3 gap-md-0 justify-content-between py-3 px-5 ">
