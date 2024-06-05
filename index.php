b864d8c14468e945cf2274fe3811bd55a4281d45
chinedugabriel <chinedugabriel29@gmail.com>
2024-06-05 03:36:36 +0100

    feat: added a button to view single page post

--- index.php
+++ index.php
@@ -75,35 +75,46 @@
                             <!-- main news content goes here -->
                             <div class="row">
                                 <div class="col-md-12">
-                                    <div class="row">
-                                        <div class="col-sm-12">
-                                            <p>
+                                    <div class="row mb-sm-3">
+                                        <div class="col-sm-12 p-sm-0">
+                                            <p class="h3">
                                             How to Get Glass Skin, According to K-beauty Experts
                                             </p>
                                         </div>
                                     </div>
                                     <div class="row">
-                                        <div class="col-sm-12">
+                                        <div class="col-sm-12 p-sm-0 text-white">
                                             <p>
-                                                BY KIANA MURDEN
+                                                <span class="bg-primary p-2">
+                                                    BY KIANA MURDEN
+                                                </span>
                                             </p>
                                         </div>
                                     </div>
                                     <div class="row">
-                                        <div class="col-sm-12">
+                                        <div class="col-sm-12 p-sm-0 text-secondary">
                                             <p>
                                                 April 24, 2024
                                             </p>
                                         </div>
                                     </div>
                                     <div class="row">
-                                        <div class="col-sm-12">
-                                            <img src="https://assets.vogue.com/photos/662819308e119cb2b91a5f87/master/w_1920,c_limit/241475549_4726893357360709_1408866255551532050_n.jpg" alt="" class="img-fluid">
+                                        <div class="col-sm-12 p-sm-0">
+                                            <img src="https://img.freepik.com/free-photo/low-angle-underground-hip-hop-musicians_23-2150932959.jpg?t=st=1717554098~exp=1717557698~hmac=bc3e6cefc413c1bd4335d57b58471e9fb87d0c9a83e048b61ac42bd60b4712ba&w=1380" alt="" class="img-fluid">
                                         </div>
                                     </div>
                                     <div class="row">
-                                        <div class="col-sm-12">
-                                            <p>brief text goes here</p>
+                                        <div class="col-sm-12 mt-sm-2 p-sm-0">
+                                            <p>
+                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus quaerat architecto voluptatibus excepturi doloribus nulla? At delectus placeat sunt labore inventore. A tenetur molestiae laboriosam nam veniam rem, mollitia fuga eos deserunt, sed iure alias ullam. Aliquam est aperiam iusto laudantium consequatur, odio commodi, expedita ipsum illum eius impedit quod!
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div class="row mb-sm-3">
+                                        <div class="col-sm-12 p-sm-0">
+                                            <button class="btn btn-outline-primary rounded-5" >
+                                                View More
+                                            </button>
                                         </div>
                                     </div>
                                 </div>
