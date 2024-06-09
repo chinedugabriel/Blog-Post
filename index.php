5e183f9bff79828aab49c3949bae9a348b6874c6
chinedugabriel <chinedugabriel29@gmail.com>
2024-06-09 16:47:09 +0100

    style: added bg tothe text in the slider

--- index.php
+++ index.php
@@ -17,10 +17,10 @@
     include_once "./include/user_menu.inc.php";
  ?>
 
-    <section class="mt-sm-3">
-        <div class="container">
+    <section class="mt-lg-3 pb-3">
+        <div class="container-lg">
             <div class="row ">
-                <div class="col-md-9">
+                <div class="col-lg-9">
                     <!-- Carousel Starts here -->
                     <div class="row">
                         <div class="col-sm-12 p-0">
@@ -32,22 +32,22 @@
                                 </div>
                                 <div class="carousel-inner">
                                     <div class="carousel-item active " data-bs-interval="10000">
-                                        <img src="https://img.freepik.com/free-photo/full-shot-underground-hip-hop-musicians_23-2150932925.jpg?t=st=1717539566~exp=1717543166~hmac=a305e5003a38c8b1434c43f4ad32ab8131efabb532355b3427f00633f99e0644&w=1380" class="d-block w-100" alt="...">
-                                        <div class="carousel-caption  d-sm-flex flex-column position-absolute top-50 start-50 translate-middle pt-4 align-items-center justify-content-center ">
+                                        <img src="https://img.freepik.com/free-photo/full-shot-underground-hip-hop-musicians_23-2150932925.jpg?t=st=1717539566~exp=1717543166~hmac=a305e5003a38c8b1434c43f4ad32ab8131efabb532355b3427f00633f99e0644&w=1380" class="d-block w-100 " alt="...">
+                                        <div class="carousel-caption  d-sm-flex flex-column position-absolute top-50 start-50 translate-middle  align-items-center justify-content-center overlay">
                                             <h5>First slide label</h5>
                                             <p>Some representative placeholder content for the first slide.</p>
                                         </div>
                                     </div>
                                     <div class="carousel-item" data-bs-interval="2000">
                                         <img src="https://img.freepik.com/free-photo/journalist-taking-interview-from-man_23-2149029312.jpg?t=st=1717540091~exp=1717543691~hmac=63d8f3a21ee97a7fda9590ff35be48d94875a69cc0e269918cd0bdafcc5b3e59&w=1380" class="d-block w-100" alt="...">
-                                        <div class="carousel-caption  d-sm-flex flex-column position-absolute top-50 start-50 translate-middle pt-4 align-items-center justify-content-center ">
+                                        <div class="carousel-caption  d-sm-flex flex-column position-absolute top-50 start-50 translate-middle  align-items-center justify-content-center overlay">
                                             <h5>Second slide label</h5>
                                             <p>Some representative placeholder content for the second slide.</p>
                                         </div>
                                     </div>
                                     <div class="carousel-item" data-bs-interval="3000">
                                         <img src="https://img.freepik.com/free-photo/full-shot-underground-hip-hop-musicians_23-2150932925.jpg?t=st=1717539566~exp=1717543166~hmac=a305e5003a38c8b1434c43f4ad32ab8131efabb532355b3427f00633f99e0644&w=1380" class="d-block w-100" alt="...">
-                                        <div class="carousel-caption  d-sm-flex flex-column position-absolute top-50 start-50 translate-middle pt-4 align-items-center justify-content-center ">
+                                        <div class="carousel-caption  d-sm-flex flex-column position-absolute top-50 start-50 translate-middle pt-4 align-items-center justify-content-center overlay">
                                             <h5>Third slide label</h5>
                                             <p>Some representative placeholder content for the third slide.</p>
                                         </div>
@@ -66,7 +66,7 @@
                     </div>
 
                     <div class="row my-4  text-primary border-bottom border-primary border-2">
-                        <div class="col-sm-12 p-sm-0">
+                        <div class="col-sm-12 p-lg-0">
                             <p class="h4">Latest News</p>
                         </div>
                     </div>
@@ -75,15 +75,15 @@
                             <!-- main news content goes here -->
                             <div class="row">
                                 <div class="col-md-12">
-                                    <div class="row mb-sm-3">
-                                        <div class="col-sm-12 p-sm-0">
+                                    <div class="row mb-2">
+                                        <div class="col-sm-12 p-lg-0">
                                             <p class="h3">
                                             How to Get Glass Skin, According to K-beauty Experts
                                             </p>
                                         </div>
                                     </div>
                                     <div class="row">
-                                        <div class="col-sm-12 p-sm-0 text-white">
+                                        <div class="col-sm-12 p-lg-0 text-white">
                                             <p>
                                                 <span class="bg-primary p-2">
                                                     BY KIANA MURDEN
@@ -92,26 +92,72 @@
                                         </div>
                                     </div>
                                     <div class="row">
-                                        <div class="col-sm-12 p-sm-0 text-secondary">
+                                        <div class="col-sm-12 p-lg-0 text-secondary">
                                             <p>
                                                 April 24, 2024
                                             </p>
                                         </div>
                                     </div>
                                     <div class="row">
-                                        <div class="col-sm-12 p-sm-0">
+                                        <div class="col-sm-12 p-lg-0">
                                             <img src="https://img.freepik.com/free-photo/low-angle-underground-hip-hop-musicians_23-2150932959.jpg?t=st=1717554098~exp=1717557698~hmac=bc3e6cefc413c1bd4335d57b58471e9fb87d0c9a83e048b61ac42bd60b4712ba&w=1380" alt="" class="img-fluid">
                                         </div>
                                     </div>
                                     <div class="row">
-                                        <div class="col-sm-12 mt-sm-2 p-sm-0">
+                                        <div class="col-sm-12 mt-sm-2 p-lg-0">
                                             <p>
-                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus quaerat architecto voluptatibus excepturi doloribus nulla? At delectus placeat sunt labore inventore. A tenetur molestiae laboriosam nam veniam rem, mollitia fuga eos deserunt, sed iure alias ullam. Aliquam est aperiam iusto laudantium consequatur, odio commodi, expedita ipsum illum eius impedit quod!
+                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus quaerat architecto voluptatibus excepturi doloribus nulla? At delectus placeat sunt labore inventore. 
                                             </p>
                                         </div>
                                     </div>
                                     <div class="row mb-sm-3">
-                                        <div class="col-sm-12 p-sm-0">
+                                        <div class="col-sm-12 p-lg-0">
+                                            <button class="btn btn-outline-primary rounded-5" >
+                                                View More
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row my-3">
+                                <div class="col-md-12">
+                                    <div class="row mb-2">
+                                        <div class="col-sm-12 p-lg-0">
+                                            <p class="h3">
+                                            How to Get Glass Skin, According to K-beauty Experts
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-sm-12 p-lg-0 text-white">
+                                            <p>
+                                                <span class="bg-primary p-2">
+                                                    BY KIANA MURDEN
+                                                </span>
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-sm-12 p-lg-0 text-secondary">
+                                            <p>
+                                                April 24, 2024
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-sm-12 p-lg-0">
+                                            <img src="https://img.freepik.com/free-photo/low-angle-underground-hip-hop-musicians_23-2150932959.jpg?t=st=1717554098~exp=1717557698~hmac=bc3e6cefc413c1bd4335d57b58471e9fb87d0c9a83e048b61ac42bd60b4712ba&w=1380" alt="" class="img-fluid">
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-sm-12 mt-sm-2 p-lg-0">
+                                            <p>
+                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus quaerat architecto voluptatibus excepturi doloribus nulla? At delectus placeat sunt labore inventore. 
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div class="row mb-sm-3">
+                                        <div class="col-sm-12 p-lg-0">
                                             <button class="btn btn-outline-primary rounded-5" >
                                                 View More
                                             </button>
@@ -124,7 +170,7 @@
                 </div>
                 <!-- Carousel ends here -->
 
-                <div class="col-md-2 offset-md-1 text-primary d-none d-md-flex flex-column position-relative " id="more-news">
+                <div class="col-md-2 offset-md-1 text-primary d-none d-lg-flex flex-column position-relative " id="more-news">
                     <!-- category goes here -->
                     <div class="row border-bottom border-primary border-2 mb-2">
                         <div class="col-sm-12 p-0">
