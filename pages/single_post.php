00004756318e9924eb9df1c1749b42ea7155a646
chinedugabriel <chinedugabriel29@gmail.com>
2024-08-23 00:23:23 +0100

    feat: added section for comments

--- pages/single_post.php
+++ pages/single_post.php
@@ -63,6 +63,86 @@
                             </p>
                         </div>
                     </div>
+                    <!-- Add comments form starts here -->
+                    <div class="row mb-3">
+                        <div class="col-md-12 p-0">
+                            <form action="" class="border px-3 py-2 d-flex flex-column gap-4" method="">
+                                <p class="h3">
+                                    Comment Here
+                                </p>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <input type="text" name="visitors_name" id="" placeholder="Name" class="form-control" required>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <input type="text" name="visitors_email" id="" placeholder="email" class="form-control" required>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <textarea name="visitors_comment" id="" class="form-control " col="" row="" placeholder="Comment">  
+                                        </textarea>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12"><button type="submit" class="btn btn-outline-dark">Submit</button></div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <!-- Add comments form ends here -->
+                     <!-- comments view starts here -->
+                      <div class="row border vh-50 ">
+                        <div class="col-md-12">
+                            <p class="border-bottom p-2 text-primary">
+                                Previous Comments
+                            </p>
+                            <!-- comments gose here -->
+                            <div class="row py-3 px-4">
+                                <div class="col-md-8 d-flex flex-column gap-3">
+                                    <!-- visitors comment shows here -->
+                                    <div class="row">
+                                        <div class="col-md-8 border">
+                                             <p>
+                                                i love the way they are dancing here..... great show
+                                             </p>
+                                             <!-- visitors Name shows here -->
+                                             <p class="text-end d-flex align-items-center gap-2">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle text-primary" viewBox="0 0 16 16">
+                                                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                                                    <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+                                                </svg>
+                                                <!-- name from database goes here -->
+                                                James Edward
+                                            </p>
+                                        </div>
+                                    </div>
+                                        <!-- visitors comment ends here -->
+
+                                    <!-- Admin's Reply goes here -->
+                                    <div class="row">
+                                        <div class="col-md-8 offset-md-1 border">
+                                             <p>
+                                                i love the way they are dancing here..... great show
+                                             </p>
+                                             <!-- visitors Name shows here -->
+                                             <p class="text-end d-flex align-items-center justify-content-end gap-2">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-check text-success" viewBox="0 0 16 16">
+                                                    <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m1.679-4.493-1.335 2.226a.75.75 0 0 1-1.174.144l-.774-.773a.5.5 0 0 1 .708-.708l.547.548 1.17-1.951a.5.5 0 1 1 .858.514M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0M8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4"/>
+                                                    <path d="M8.256 14a4.5 4.5 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10q.39 0 .74.025c.226-.341.496-.65.804-.918Q8.844 9.002 8 9c-5 0-6 3-6 4s1 1 1 1z"/>
+                                                </svg>
+                                                <!-- name from database goes here -->
+                                                James Edward
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <!-- Admin's Reply goes here -->
+                                </div>
+                            </div>
+                        </div>
+                      </div>
+                     <!-- comments view ends here -->
+
 
                     <!-- Related post starts here -->
                     <div class="row mt-2 mb-4 text-primary border-bottom border-primary border-2">
