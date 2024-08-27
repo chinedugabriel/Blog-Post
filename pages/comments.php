97f42fba3a9835320673c37d05b6a70f219b8aa1
chinedugabriel <chinedugabriel29@gmail.com>
2024-08-27 01:17:59 +0100

    file: added model files, controller files and changed the title for all pages

--- pages/comments.php
+++ pages/comments.php
@@ -13,7 +13,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog Post</title>
+    <title>Comments</title>
     <link rel="stylesheet" href="../style.css">
     <!-- bootstrap link start here 
     * Bootstrap v5.3.0-alpha3 
--- pages/single_post.php
+++ pages/single_post.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog Post</title>
+    <title>Single Post</title>
     <link rel="stylesheet" href="blog.css">
     <!-- bootstrap link start here 
     * Bootstrap v5.3.0-alpha3 
